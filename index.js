cf38583cee1d6148a53c586e4830d8963b55c377
YodaCode <ylsbr@googlegroups.com>
2021-03-29 02:41:14 +0000

    changes

--- index.js
+++ index.js
@@ -20,14 +20,17 @@ app.get('/', (req, res) => {
     }
   });
 });
+
 app.get('/messages', async (req, res) => {
   var data = await db.get('responses');
   res.send(JSON.stringify(data));
 });
+
 app.get('/messages/clear', async (req, res) => {
   await db.set('responses', []);
   res.send(JSON.stringify([]));
 });
+
 app.post('/', async (req, res) => {
   await db.push('responses', {
     name: req.body.name,
@@ -42,6 +45,7 @@ app.post('/', async (req, res) => {
     }
   });
 });
+
 app.listen(8080, () => {
   console.log('Listening on *:8080');
 });
